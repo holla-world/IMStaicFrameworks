6e5c11341c30ba577d869a935575aeefeb94103e
maopenglin <maopenglin@gmail.com>
2021-12-23 18:57:53 +0800

    init

---
+++ IMMessageSDK.framework/Headers/IMMessageSDK.h
@@ -0,0 +1,18 @@
+//
+//  IMMessageSDK.h
+//  IMMessageSDK
+//
+//  Created by admin on 23-12-21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IMMessageSDK.
+FOUNDATION_EXPORT double IMMessageSDKVersionNumber;
+
+//! Project version string for IMMessageSDK.
+FOUNDATION_EXPORT const unsigned char IMMessageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMMessageSDK/PublicHeader.h>
+
+
